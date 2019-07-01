e9f7397c8b4de2b43b9ebc609642ce837d2aaf6b
Amaru Fernandez <amarud@gmail.com>
2019-07-01 19:46:30 -0400

    se agrega texto

--- www/utils/functions.js
+++ www/utils/functions.js
@@ -102,19 +102,53 @@ function initMap() {
 
       includeHTML();
 
-      document.getElementById("btn1").onclick = function () { CambioImagen(); $('#1957').show();};
-      document.getElementById("btn2").onclick = function () { CambioImagen(); $('#1960').show();};
-      document.getElementById("btn3").onclick = function () { CambioImagen(); $('#1970').show();};
-      document.getElementById("btn4").onclick = function () { CambioImagen(); $('#19731').show();};
-      document.getElementById("btn5").onclick = function () { CambioImagen(); $('#19732').show();};
-      document.getElementById("btn6").onclick = function () { CambioImagen(); $('#19733').show();};
-      document.getElementById("btn7").onclick = function () { CambioImagen(); $('#197376').show();};
-      document.getElementById("btn8").onclick = function () { CambioImagen(); $('#197681').show();};
+      document.getElementById("btn1").onclick = function () { 
+          CambioImagen();
+          $('#img1957').show();
+          $('#con1957').show();
+        };
+      document.getElementById("btn2").onclick = function () { 
+          CambioImagen(); 
+          $('#img1960').show();
+          $('#con1960').show();
+        };
+      document.getElementById("btn3").onclick = function () { 
+          CambioImagen(); 
+          $('#img1970').show();
+          $('#con1970').show();
+        };
+      document.getElementById("btn4").onclick = function () { 
+          CambioImagen(); 
+          $('#img19731').show();
+          $('#con19731').show();
+        };
+      document.getElementById("btn5").onclick = function () { 
+          CambioImagen(); 
+          $('#img19732').show();
+          $('#con19732').show();
+        };
+      document.getElementById("btn6").onclick = function () { 
+          CambioImagen(); 
+          $('#img19733').show();
+          $('#con19733').show();
+        };
+      document.getElementById("btn7").onclick = function () { 
+          CambioImagen(); 
+          $('#img197376').show();
+          $('#con197376').show();
+        };
+      document.getElementById("btn8").onclick = function () { 
+          CambioImagen(); 
+          $('#img197681').show();
+          $('#con197681').show();
+        };
 
       function CambioImagen(){
         $('.disnon').hide();
       }
+
       CambioImagen();
-      $('#1957').show();
+      $('#img1957').show();
+      $('#con1957').show();
 
 }
