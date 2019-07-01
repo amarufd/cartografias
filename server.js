d746604c61bcc2a0758e3b8c04718cef38025b44
Amaru Fernandez <amaru@localhost.localdomain>
2019-07-01 13:17:11 -0400

    3er intento

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-var express = require('express'),
-app = express();
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+var http = require('http'),
+    fs = require('fs')
+var port = process.env.PORT || 3000
+
+
+http.createServer(function(req, res) {
+    var url = './' + (req.url == 'www/' ? 'www/index.html' : req.url)
+    fs.readFile(url, function(err, html) {
+        if (err) {
+            var message404 = "There is no such page! <a href='/'>Back to home page</a>"
+            res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': message404.length})
+            res.write(message404)
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': html.length})
+            res.write(html)
+        }
+        res.end()
+    })
+}).listen(port)
